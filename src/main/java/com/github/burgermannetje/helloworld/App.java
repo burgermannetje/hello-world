f61f57a6a32e82cd538019617708093efca07f3b
Jeroen Mertens <burgermannetje@gmail.com>
2016-04-07 15:22:59 +0200

    Initial project files

---
+++ src/main/java/com/github/burgermannetje/helloworld/App.java
@@ -0,0 +1,9 @@
+package com.github.burgermannetje.helloworld;
+
+public class App {
+
+	public Object getMessage(String name) {
+		return "Hello "+ name + "!";
+	}
+
+}
---
+++ src/test/java/com/github/burgermannetje/helloworld/AppTest.java
@@ -0,0 +1,25 @@
+package com.github.burgermannetje.helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void getMessage() throws Exception {
+		App app = new App();
+		
+		String name = "Jeroen";
+		assertEquals(app.getMessage(name), "Hello Jeroen!");
+	}
+
+	@Test
+	public void getMessageTjeerd() throws Exception {
+		App app = new App();
+		
+		String name = "Tjeerd";
+		assertEquals(app.getMessage(name), "Hello Tjeerd!");
+	}
+
+}
